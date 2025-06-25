743f276d47178eb20f5123201e7c7e0fa2252461
Patrick Kaiser <patrick.kaiser@fau.de>
2025-06-25 15:45:55 +0200

    husky test

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,7 @@ app.get('/api/health', async (req, res) => {
 // Body parser
 app.use(express.json({ limit: '50mb' }));
 
-var  foo=42; console.log("Foo",foo)
+var  foo=42; console.log("FooBar",foo)
 
 // 1) Load user (sets req.user if token valid)
 app.use(loadUser);
